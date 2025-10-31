c9865dba3e9a9cbaf07d38f12e154c026bcf605d
Maha065 <maha060506@gmail.com>
2025-10-31 21:36:00 +0100

    Refactor property_list to use utility function
    Refactor property_list view to use get_all_properties utility for fetching properties and serialize the response for JSON.

--- properties/views.py
+++ properties/views.py
@@ -1,11 +1,18 @@
 from django.http import JsonResponse
-from django.views.decorators.cache import cache_page
-from .models import Property
+from .utils import get_all_properties
 
-# Cache the response for 15 minutes (60 sec * 15)
-@cache_page(60 * 15)
 def property_list(request):
-    properties = Property.objects.all().values(
-        'id', 'title', 'description', 'price', 'location', 'created_at'
-    )
-    return JsonResponse(list(properties), safe=False)
+    properties = get_all_properties()
+    # Serialize for JSON response
+    properties_data = [
+        {
+            'id': p.id,
+            'title': p.title,
+            'description': p.description,
+            'price': str(p.price),
+            'location': p.location,
+            'created_at': p.created_at.isoformat()
+        }
+        for p in properties
+    ]
+    return JsonResponse(properties_data, safe=False)
